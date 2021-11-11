b7e46f8bd4624ad2d23e8d33cef61f2848869d3b
Rike <rike.vaneo@ti.ukdw.ac.id>
2021-11-11 19:58:06 +0700

    menambahkan soal 3

---
+++ 3_E_71210787.py
@@ -0,0 +1,7 @@
+belanja1=input("Masukkan daftar belanja 1: ")
+belanja2=input("Masukkan daftar belanja 2: ")
+tambah1=input("Tambahkan data ke daftar belanja 1: ")
+tambah2=input("Tambahkan data ke daftar belanja 2: ")
+
+print("Daftar belanja 1 adalah",belanja1+','+tambah1)
+print("Daftar belanja 2 adalah",belanja2+','+tambah2)
