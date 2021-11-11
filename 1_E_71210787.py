ebe64ae769a6bc85fb1cd1d5f138f51862991fe4
Rike <rike.vaneo@ti.ukdw.ac.id>
2021-11-11 19:56:23 +0700

    menambahkan soal 1

---
+++ 1_E_71210787.py
@@ -0,0 +1,7 @@
+alas1=int(input("Masukkan alas atas : ")) 
+alas2=int(input("Masukkan alas bawah : "))
+tinggi=int(input("Masukkan tinggi : "))
+
+luas=0.5*(alas1+alas2)*tinggi
+
+print("Luas trapesium adalah :",luas)
